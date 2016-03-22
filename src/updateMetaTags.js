0835961074220d8acf8fcd9a6541ac049bfceb7e
StevenIseki <stevenisekimartin@gmail.com>
2016-03-22 12:18:21 +1100

    update to comments

--- src/updateMetaTags.js
+++ src/updateMetaTags.js
@@ -12,15 +12,11 @@ deafult export function updateMetaTags (title, descrition) {
     /* remove the old routes meta tags */
     removeElementsByClass('meta')
     
-    /* title */
     document.title = title
-
-    /* description */
     var meta = document.createElement('meta')
     meta.name = 'descrition'
     meta.content = descrition
     meta.className = 'meta'
     head.appendChild(meta)
-
   }
 }
